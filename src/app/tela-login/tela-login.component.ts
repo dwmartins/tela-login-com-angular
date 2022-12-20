577ff749172d255fdfc50dbb1e1281b8e0d33f2c
Douglas Martins <douglas5422@outlook.com>
2022-12-20 16:09:49 -0300

    Feat: validações para vizualizar a senha no input

--- src/app/tela-login/tela-login.component.ts
+++ src/app/tela-login/tela-login.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tela-login',
   templateUrl: './tela-login.component.html',
   styleUrls: ['./tela-login.component.css']
 })
-export class TelaLoginComponent {
+export class TelaLoginComponent implements OnInit {
 
+  tipoInput: string = "password";
+  aberto: boolean = false;
+
+  public iconEye(): void {
+    if(this.aberto == false) {
+      this.aberto = true;
+      this.tipoInput = "text";
+    } else {
+      this.aberto = false;
+      this.tipoInput= "password";
+    }
+  }
+
+  constructor(){
+
+  }
+
+  ngOnInit() {
+  }
 }
